03a1dfeacdf04ea8b3f49b9c833e7ee61e4379f7
Prateek Bahad <prateekbahad@gmail.com>
2026-06-22 23:49:02 +0530

    update server

--- Controller/userController.js
+++ Controller/userController.js
@@ -62,7 +62,7 @@ export const login = async (req, res) => {
 
     res.cookie("authToken", token, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: "none",
       path: "/",
     });
@@ -148,7 +148,7 @@ export const logout = async (req, res) => {
   try {
     res.clearCookie("authToken", {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: "none",
       path: "/",
     });
