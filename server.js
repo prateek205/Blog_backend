cb115be7541ed8619b101714cd881c6cc6d7eb40
Prateek Bahad <prateekbahad@gmail.com>
2026-06-22 21:27:10 +0530

    update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://blog-frontend-p9ru.onrender.com"],
+    origin: ["http://localhost:5173", "https://blog-frontend-21oi.onrender.com"],
     credentials: true,
   }),
 );
